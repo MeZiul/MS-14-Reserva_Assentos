595dd2159f1bb922ce4401a3ece444c909bfa87b
MeZiul <luizguilhermy83302@gmail.com>
2026-09-13 21:45:30 -0400

    feature: Tutorial  prisma; Chore: mudança para .js

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.ts
+import 'dotenv/config';
+import http from 'node:http';
+import app, { appPronto } from './src/app.js';
+
+const port = Number(process.env.APP_PORT ?? 7340);
+
+await appPronto;
+const httpServer = http.createServer(app);
+httpServer.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
+});
